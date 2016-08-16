80470d0f2828bdd174c658a17704ee9656e55394
Yehor Nazarkin <nimnull@gmail.com>
2016-08-17 00:45:58 +0300

    Defined collection update interface call

--- crap/db.py
+++ crap/db.py
@@ -1,16 +1,19 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
+from crap.logs import logger
+
 
 class Storage:
 
     def __init__(self, mongo_uri):
-        self.db = AsyncIOMotorClient(mongo_uri)
+        self.mongo_client = AsyncIOMotorClient(mongo_uri)
+        self.db = self.mongo_client.get_default_database()
 
     def find(self, collection):
         pass
 
-    def update(self, collection):
-        pass
+    async def update(self, collection, query, update, options):
+        return await self.db[collection].update(query, update, **options)
 
     def insert(self, collection, document):
         pass
